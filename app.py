4a7bf5eb2c44caba0a7cb344ae33bdb24520b20e
Igor Cortes Alves Junqueira <igorjunqueiraq@MacBook-Pro-de-Igor.local>
2026-06-08 14:20:48 -0300

    first app structure

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mostra Tec UNIUBE 2026", layout="centered")
+
+st.title("🔬 Sistema de Avaliação - UNIUBE")
+st.write("Seu app está online! Próximo passo: Configurar o banco de dados e o login.")
